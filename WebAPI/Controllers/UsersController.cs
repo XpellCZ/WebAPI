49ce0349b3a4875505c15f83de3c20aa92787c01
Marek Poul <poul.marek@gmail.com>
2018-12-22 18:23:46 +0100

    CORS enabled

--- WebAPI/Controllers/UsersController.cs
+++ WebAPI/Controllers/UsersController.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using WebAPI.Models;
+
 
 namespace WebAPI.Controllers
 {
+
+    [EnableCors(origins: "http://localhost:50328", headers: "*", methods: "*")]
     public class UsersController : ApiController
     {
         private TestContext context;
