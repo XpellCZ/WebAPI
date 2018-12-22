537e5446aaea4f82bb92bd597c086d36f73cd269
Marek Poul <poul.marek@gmail.com>
2018-12-22 18:26:04 +0100

    CORS enabled on HomeController

--- WebAPI/Controllers/HomeController.cs
+++ WebAPI/Controllers/HomeController.cs
@@ -2,10 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http.Cors;
 using System.Web.Mvc;
 
 namespace WebAPI.Controllers
 {
+
+    [EnableCors(origins: "http://localhost:50328", headers: "*", methods: "*")]
     public class HomeController : Controller
     {
         public ActionResult Index()
