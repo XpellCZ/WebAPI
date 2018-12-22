5cc40de2dbdf549fbd6f34cebe7927d3aeab80d8
Marek Poul <poul.marek@gmail.com>
2018-12-22 16:02:26 +0100

    Initial commit

---
+++ WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class UsersController : ApiController
+    {
+        private TestContext context;
+
+
+        public UsersController()
+        {
+            context = new TestContext();
+
+        }
+
+        public List<Users> Post() {
+
+            return this.context.Users.ToList();
+
+
+        }
+
+    }
+}
---
+++ WebAPI/Models/Users.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Models
+{
+    public class Users
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+
+    }
+}
