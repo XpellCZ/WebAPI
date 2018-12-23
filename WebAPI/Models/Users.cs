41fd3e60742f1888ba28d8b394b3e108a312e704
Marek Poul <poul.marek@gmail.com>
2018-12-23 16:20:33 +0100

    Edit of model Users

--- WebAPI/Models/Users.cs
+++ WebAPI/Models/Users.cs
@@ -9,7 +9,8 @@ namespace WebAPI.Models
     {
 
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string Username { get; set; }
+        public string FullName { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Description { get; set; }
